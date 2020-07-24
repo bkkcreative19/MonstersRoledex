e635138df01213185339038bbcc6f0d28bc31a7c
Baron King <bkkcreative17@gmail.com>
2020-07-24 16:46:31 -0500

    Updates

---
+++ precache-manifest.d177ac1d85b6f94638cad647aa095226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8da65448498f48359a4d4d9087e939",
+    "url": "/MonstersRoledex/index.html"
+  },
+  {
+    "revision": "409c4ba936307efa09e0",
+    "url": "/MonstersRoledex/static/css/main.eaaad769.chunk.css"
+  },
+  {
+    "revision": "9ec93ce0e1c5ba2412a6",
+    "url": "/MonstersRoledex/static/js/2.cd8b7b2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRoledex/static/js/2.cd8b7b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409c4ba936307efa09e0",
+    "url": "/MonstersRoledex/static/js/main.2720a2cd.chunk.js"
+  },
+  {
+    "revision": "8940b6be2983265fc368",
+    "url": "/MonstersRoledex/static/js/runtime-main.d68bc98a.js"
+  }
+]);
